b318bfe8c6af86db0f87202dc7c482c2264245fd
Asgeir Frimannsson <asgeirf@gmail.com>
2013-02-04 22:23:14 +0100

    Move BOM definition out of plugin

--- src/main/groovy/org/jboss/arquillian/gradle/ArquillianSetupPlugin.groovy
+++ src/main/groovy/org/jboss/arquillian/gradle/ArquillianSetupPlugin.groovy
@@ -6,13 +6,14 @@ import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.gradle.api.plugins.JavaPlugin
 
+import static org.jboss.arquillian.gradle.BillOfMaterial.ARQUILLIAN_BOM
+
 class ArquillianSetupPlugin implements Plugin<Project>, ConfigurationHandler<ArquillianSetupPluginConvention> {
     private static final Logger logger = Logging.getLogger(ArquillianSetupPlugin)
 
     private Map<String, ContainerConfig> supportedContainers = new HashMap<String, ContainerConfig>()
     private Map<String, String> supportedExtensions = new HashMap<String,String>()
 
-    private BillOfMaterial bom = createArquillianBom()
     private Project project
 
     public ArquillianSetupPlugin() {
@@ -36,13 +37,13 @@ class ArquillianSetupPlugin implements Plugin<Project>, ConfigurationHandler<Arq
 
     private void configureJunit() {
         project.dependencies {
-            testCompile bom.resolve('org.jboss.arquillian.junit:arquillian-junit-container')
+            testCompile ARQUILLIAN_BOM.resolve('org.jboss.arquillian.junit:arquillian-junit-container')
         }
     }
 
     private void configureTestNg() {
         project.dependencies {
-            testCompile bom.resolve('org.jboss.arquillian.testng:arquillian-testng-container')
+            testCompile ARQUILLIAN_BOM.resolve('org.jboss.arquillian.testng:arquillian-testng-container')
         }
     }
 
@@ -62,86 +63,6 @@ class ArquillianSetupPlugin implements Plugin<Project>, ConfigurationHandler<Arq
 
     private void addContainer(ContainerConfig containerConfig) {
         supportedContainers.put(containerConfig.id, containerConfig)
-    }
-
-    private static BillOfMaterial createArquillianBom() {
-        Map<String, String> versions = [
-                arquillian: '1.0.3.Final',
-                shrinkwrap_shrinkwrap: '1.0.1',
-                shrinkwrap_descriptors: '2.0.0-alpha-3',
-                shrinkwrap_resolver: '1.0.0-beta-7'
-        ]
-
-        BillOfMaterial arqBom = new BillOfMaterial('org.jboss.arquillian', 'arquillian-bom', versions.arquillian,
-                [
-                        // core
-                        'org.jboss.arquillian.core:arquillian-core-api',
-                        'org.jboss.arquillian.core:arquillian-core-spi',
-                        'org.jboss.arquillian.core:arquillian-core-impl-base',
-                        // config
-                        'org.jboss.arquillian.config:arquillian-config-api',
-                        'org.jboss.arquillian.config:arquillian-config-spi',
-                        'org.jboss.arquillian.config:arquillian-config-impl-base',
-                        // test
-                        'org.jboss.arquillian.test:arquillian-test-api',
-                        'org.jboss.arquillian.test:arquillian-test-spi',
-                        'org.jboss.arquillian.test:arquillian-test-impl-base',
-                        // container
-                        'org.jboss.arquillian.container:arquillian-container-api',
-                        'org.jboss.arquillian.container:arquillian-container-spi',
-                        'org.jboss.arquillian.container:arquillian-container-impl-base',
-                        // container test
-                        'org.jboss.arquillian.container:arquillian-container-test-api',
-                        'org.jboss.arquillian.container:arquillian-container-test-spi',
-                        'org.jboss.arquillian.container:arquillian-container-test-impl-base',
-                        // junit
-                        'org.jboss.arquillian.junit:arquillian-junit-core',
-                        'org.jboss.arquillian.junit:arquillian-junit-container',
-                        'org.jboss.arquillian.junit:arquillian-junit-standalone',
-                        // testng
-                        'org.jboss.arquillian.testng:arquillian-testng-core',
-                        'org.jboss.arquillian.testng:arquillian-testng-container',
-                        'org.jboss.arquillian.testng:arquillian-testng-standalone',
-                        // protocols
-                        'org.jboss.arquillian.protocol:arquillian-protocol-servlet',
-                        'org.jboss.arquillian.protocol:arquillian-protocol-jmx',
-                        // enrichers
-                        'org.jboss.arquillian.testenricher:arquillian-testenricher-cdi',
-                        'org.jboss.arquillian.testenricher:arquillian-testenricher-ejb',
-                        'org.jboss.arquillian.testenricher:arquillian-testenricher-resource',
-                        'org.jboss.arquillian.testenricher:arquillian-testenricher-initialcontext'
-                ],
-                [
-                        new BillOfMaterial('org.jboss.shrinkwrap', 'shrinkwrap-bom', versions.shrinkwrap_shrinkwrap,
-                                [
-                                        'org.jboss.shrinkwrap:shrinkwrap-api',
-                                        'org.jboss.shrinkwrap:shrinkwrap-spi',
-                                        'org.jboss.shrinkwrap:shrinkwrap-impl-base'
-                                ]
-                        ),
-                        new BillOfMaterial('org.jboss.shrinkwrap.resolver', 'shrinkwrap-resolver-bom', versions.shrinkwrap_resolver,
-                                [
-                                        'org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api',
-                                        'org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api-maven',
-                                        'org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven'
-                                ]
-                        ),
-                        new BillOfMaterial('org.jboss.shrinkwrap.descriptors', 'shrinkwrap-descriptors-bom', versions.shrinkwrap_descriptors,
-                                [
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-api-base',
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-api-javaee',
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-api-jboss',
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-gen',
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-impl-base',
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-impl-javaee',
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-impl-jboss',
-                                        'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-spi'
-                                ]
-                        )
-                ]
-        )
-
-        return arqBom
     }
 
 }
--- src/main/groovy/org/jboss/arquillian/gradle/BillOfMaterial.groovy
+++ src/main/groovy/org/jboss/arquillian/gradle/BillOfMaterial.groovy
@@ -30,4 +30,82 @@ class BillOfMaterial {
         }
         throw new IllegalArgumentException("artifact not found: ${artifact}")
     }
+
+    private static final Map<String, String> ARQUILLIAN_VERSIONS = [
+                    arquillian: '1.0.3.Final',
+                    shrinkwrap_shrinkwrap: '1.0.1',
+                    shrinkwrap_descriptors: '2.0.0-alpha-3',
+                    shrinkwrap_resolver: '1.0.0-beta-7'
+    ]
+
+    public static final BillOfMaterial ARQUILLIAN_BOM =
+        new BillOfMaterial('org.jboss.arquillian', 'arquillian-bom', ARQUILLIAN_VERSIONS.arquillian,
+                    [
+                            // core
+                            'org.jboss.arquillian.core:arquillian-core-api',
+                            'org.jboss.arquillian.core:arquillian-core-spi',
+                            'org.jboss.arquillian.core:arquillian-core-impl-base',
+                            // config
+                            'org.jboss.arquillian.config:arquillian-config-api',
+                            'org.jboss.arquillian.config:arquillian-config-spi',
+                            'org.jboss.arquillian.config:arquillian-config-impl-base',
+                            // test
+                            'org.jboss.arquillian.test:arquillian-test-api',
+                            'org.jboss.arquillian.test:arquillian-test-spi',
+                            'org.jboss.arquillian.test:arquillian-test-impl-base',
+                            // container
+                            'org.jboss.arquillian.container:arquillian-container-api',
+                            'org.jboss.arquillian.container:arquillian-container-spi',
+                            'org.jboss.arquillian.container:arquillian-container-impl-base',
+                            // container test
+                            'org.jboss.arquillian.container:arquillian-container-test-api',
+                            'org.jboss.arquillian.container:arquillian-container-test-spi',
+                            'org.jboss.arquillian.container:arquillian-container-test-impl-base',
+                            // junit
+                            'org.jboss.arquillian.junit:arquillian-junit-core',
+                            'org.jboss.arquillian.junit:arquillian-junit-container',
+                            'org.jboss.arquillian.junit:arquillian-junit-standalone',
+                            // testng
+                            'org.jboss.arquillian.testng:arquillian-testng-core',
+                            'org.jboss.arquillian.testng:arquillian-testng-container',
+                            'org.jboss.arquillian.testng:arquillian-testng-standalone',
+                            // protocols
+                            'org.jboss.arquillian.protocol:arquillian-protocol-servlet',
+                            'org.jboss.arquillian.protocol:arquillian-protocol-jmx',
+                            // enrichers
+                            'org.jboss.arquillian.testenricher:arquillian-testenricher-cdi',
+                            'org.jboss.arquillian.testenricher:arquillian-testenricher-ejb',
+                            'org.jboss.arquillian.testenricher:arquillian-testenricher-resource',
+                            'org.jboss.arquillian.testenricher:arquillian-testenricher-initialcontext'
+                    ],
+                    [
+                            new BillOfMaterial('org.jboss.shrinkwrap', 'shrinkwrap-bom', ARQUILLIAN_VERSIONS.shrinkwrap_shrinkwrap,
+                                    [
+                                            'org.jboss.shrinkwrap:shrinkwrap-api',
+                                            'org.jboss.shrinkwrap:shrinkwrap-spi',
+                                            'org.jboss.shrinkwrap:shrinkwrap-impl-base'
+                                    ]
+                            ),
+                            new BillOfMaterial('org.jboss.shrinkwrap.resolver', 'shrinkwrap-resolver-bom', ARQUILLIAN_VERSIONS.shrinkwrap_resolver,
+                                    [
+                                            'org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api',
+                                            'org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api-maven',
+                                            'org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven'
+                                    ]
+                            ),
+                            new BillOfMaterial('org.jboss.shrinkwrap.descriptors', 'shrinkwrap-descriptors-bom', ARQUILLIAN_VERSIONS.shrinkwrap_descriptors,
+                                    [
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-api-base',
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-api-javaee',
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-api-jboss',
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-gen',
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-impl-base',
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-impl-javaee',
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-impl-jboss',
+                                            'org.jboss.shrinkwrap.descriptors:shrinkwrap-descriptors-spi'
+                                    ]
+                            )
+                    ]
+            )
+
 }
